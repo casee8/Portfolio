55c44fe8f426dddc93d782f7db5528fdfa7b137b
casee8 <pokerkaszy7@gmail.com>
2018-06-07 11:35:07 +0100

    Add smooth scrolling

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+// Animate smooth scroll | not really a DRY code...
+$(".about").on('click', function(){
+  const about = $('#about').position().top;
+
+  $('html, body').animate({
+    scrollTop: about
+  }, 500);
+});
+
+$(".portfolio").on('click', function(){
+  const portfolio = $('#portfolio').position().top;
+
+  $('html, body').animate({
+    scrollTop: portfolio
+  }, 500);
+});
+
+$('.contact').on('click', function(){
+  const contact = $('#contact').position().top;
+
+  $('html, body').animate({
+    scrollTop: contact
+  }, 500);
+});
+
+$('.fa-chevron-circle-up').on('click', function(){
+  const pageTop = $('.pageTop').position().top;
+
+  $('html, body').animate({
+    scrollTop: pageTop
+  }, 300);
+});
