8aaeb6ca22d31f58816b0b67ae7541422af505c0
casee8 <pokerkaszy7@gmail.com>
2018-11-22 10:48:51 +0000

    Added new contact option/Updated content

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,3 +53,11 @@ function cursorBlinking(){
   }
 }
 
+// PeoplePerHour Widget
+(function(d, s) {
+  var useSSL = 'https:' == document.location.protocol;
+  var js, where = d.getElementsByTagName(s)[0],
+  js = d.createElement(s);
+  js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/51472059/2477149.js?width=300&height=135&orientation=vertical&theme=dark&rnd='+parseInt(Math.random()*10000, 10);
+  try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
+}(document, 'script'));
